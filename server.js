eb3d33375e7630ef73b0d8d1153e7f98006f6e46
Young_programmer <134650124+olekskz@users.noreply.github.com>
2025-01-11 19:59:46 +0100

    Update server.js

--- server.js
+++ server.js
@@ -24,26 +24,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const express = require('express')
-const PORT  = 3000;
-const app = express()
-const path = require('path')
-const helmet = require('helmet')
-const passport = require('passport')
-const localStrategy = require('passport-local').Strategy
-const session = require('express-session')
-const {User} = require('./models')
-require('dotenv').config()
-const photoRoutes = require('./routers/photo-route');
-const userRoutes = require('./routers/user-router');
-const bcrypt = require('bcrypt')
-const cors = require('cors');
-
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended: true}));
-
-
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
